5ad8976a76864053e371e42ceeedab3733a1bdf2
1752146907@qq.com <1752146907@qq.com>
2020-08-19 15:21:47 +0800

    未渲染添加loding效果

--- src/index.js
+++ src/index.js
@@ -48,6 +48,8 @@ ReactDOM.render(
         {/*</Router>*/}
     </HashRouter>, document.getElementById('root'));
 
+document.getElementById("loading").remove();
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
